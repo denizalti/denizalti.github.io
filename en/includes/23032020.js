7819b731e26e78fa1e39fa4e007cf0577f4898a2
denizalti <1463760+denizalti@users.noreply.github.com>
2020-08-01 16:07:50 -0700

    Remove old documents and add email.

--- en/includes/23032020.js
+++
@@ -1,4 +0,0 @@
-document.write("\
-<b>March 23, 2020</b><br>\
-  Made the English content public.\
-  Added pictures and sources to all questions.");
--- tr/load_csv.js
+++
@@ -1,3 +0,0 @@
-
-console.log("Calling initialize;");
-initialize();
